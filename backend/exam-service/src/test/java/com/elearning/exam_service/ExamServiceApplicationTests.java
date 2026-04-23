65bea1c496ae860628313f24975a0cd0592efd76
WANKO Ramses <ramsesjr14@gmail.com>
2026-04-23 12:52:20 +0100

    fix: create test profile config and add @ActiveProfiles to context test

--- backend/exam-service/src/test/java/com/elearning/exam_service/ExamServiceApplicationTests.java
+++ backend/exam-service/src/test/java/com/elearning/exam_service/ExamServiceApplicationTests.java
@@ -2,12 +2,14 @@ package com.elearning.exam_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ExamServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
